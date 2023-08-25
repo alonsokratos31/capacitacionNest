68cf539125dd7701416ae7e717d9fbd392b560cd
alonsokratos31 <alonso.garcia025@gmail.com>
2023-08-25 12:17:34 -0400

    Se modifica permisos

--- src/permisos/permisos.controller.ts
+++ src/permisos/permisos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PermisosService } from './permisos.service';
 import { CreatePermisoDto } from './dto/create-permiso.dto';
 import { UpdatePermisoDto } from './dto/update-permiso.dto';
